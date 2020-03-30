c6a4764a0c172da8c2138e1f5f68d5f5f732e7b9
Lourdes <lourdes.rm10@gmail.com>
2020-03-30 13:07:52 -0500

    aggregation update plot 3 4 8

--- test_app3.js
+++ test_app3.js
@@ -6,22 +6,20 @@ d3.json(path).then(function(data){
     var accident = data.map(d=>d.PER_TYP);
         console.log(helmetUse)
 
-    var trace = {
-        x: helmetUse,
-        y: accident,
-        type: "bar",
-        transforms: {
-            type: 'aggregate',
-            groups: helmetUse,
-            aggregations: 
-              {target: 'y', func: 'sum', enabled: true},
-            
-          }
+    var data1 = {
+            type: 'bar',
+            x: helmetUse,
+            y: accident,
+            // mode: 'lines+markers',
+            transforms: [{
+             type: 'aggregate',
+             groups: helmetUse,
+             aggregations: [
+                {target: 'y', func: 'sum', enabled: true},
+        ]
+      }]
     };
 
-    var data = [trace];
-    var layout = {
-        title: "Test 3"
-    };
-    Plotly.newPlot("plot", data, layout);
-})
+    var traces = [data1]
+    Plotly.newPlot("plot", traces);
+});
--- test_app4.js
+++ test_app4.js
@@ -2,26 +2,26 @@
 var path = 'Cleaned/Master_filtered.json'
 d3.json(path).then(function(data){
     
-    var drink = data.map(d=>d.DRINKING);
-    var accident = data.map(d=>d.PER_TYP);
+    var drinkRecord = data.filter(d=>["Yes", "No"].indexOf(d.DRINKING) != -1);
+    var accident = drinkRecord.map(d=>d.PER_TYP);
+    var drink = drinkRecord.map(d=>d.DRINKING)
         console.log(drink)
 
-    var trace = {
-        x: drink,
-        y: accident,
-        type: "bar",
-        transforms: {
-            type: 'aggregate',
-            groups: drink,
-            aggregations: 
-              {target: 'y', func: 'sum', enabled: true},
-            
-          }
+        var data1 = {
+            type: 'bar',
+            x: drink,
+            y: accident,
+            // mode: 'lines+markers',
+            transforms: [{
+             type: 'aggregate',
+             groups: drink,
+             aggregations: [
+                {target: 'y', func: 'sum', enabled: true},
+        ]
+      }]
     };
-
-    var data = [trace];
-    var layout = {
-        title: "Test 4"
-    };
-    Plotly.newPlot("plot", data, layout);
+    
+    
+    var traces = [data1]
+    Plotly.newPlot("plot", traces);
 })
